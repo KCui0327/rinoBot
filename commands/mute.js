4a031b8f05a0b6095788e3b0d0a696fbdc695044
KCui0327 <kenny.cui@mail.utoronto.ca>
2022-08-05 23:23:25 -0700

    created mute file

--- commands/mute.js
+++ commands/mute.js
@@ -1,7 +1,12 @@
 module.exports = {
     name: "mute",
     description: "mutes discord users",
-    execute(message, args) {
-        message.channel.send("mute!");
+    execute(msg, args) {
+        const muteUser = msg.mentions.users.first();
+        if (muteUser) {
+            let targetRole = msg.guild.roles.cache.find(role => role.name === "")
+        } else {
+            msg.channel.send("Cannot find user.")
+        }
     }
 }
--- main.js
+++ main.js
@@ -1,35 +1,29 @@
 const Discord = require("discord.js");
-
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-
+const client = new Discord.Client();
 const prefix = "!";
-
-const fs = require("fs");
+const fs = require("fs")
+require("dotenv").config();
 
 client.commands = new Discord.Collection();
-
-const commandOperations = fs.readdirSync("./commands/").filter(file => file.endsWith(".js"));
-for (const file of commandOperations) {
+const commandFiles = fs.readdirSync("./commands/").filter(file => file.endsWith(".js"));
+for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
 
-client.once("ready", () => {
+client.on("ready", () => {
     console.log("Online");
 });
 
-client.on("message", message => {
-    if (!message.content.startsWith(prefix) || message.authorbot) {
-        console.log("Wrong Prefix! Please use" + " '!'.");
-        return;
-    }
+client.on("message", msg =>{
+    if(!msg.content.startsWith(prefix) || msg.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
+    const args = msg.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if(command === "mute") {
-        client.commands.get("mute").execute(message, args);
+    if (command === "mute") {
+        client.commands.get("mute").execute(msg, args)
     }
-})
+});
 
-client.login("OTk5NzgzODE0MDA2MDYzMjE0.GMzKlX.Jpf5xXGj_K0bMc1s4DkzowbpMlks6k1W8xgecA");
+client.login(process.env.DISCORD_TOKEN);
