421ac3dfd1054347c40423eef74fbdfe1da5eb15
TheKCui0327 <59521762+KCui0327@users.noreply.github.com>
2022-09-02 19:27:04 -0400

    Made comments

--- commands/songControl.js
+++ commands/songControl.js
@@ -1,7 +1,7 @@
 const ytdl = require("ytdl-core");
 const ytSearch = require("yt-search");
 
-const musicList = new Map();
+const musicList = new Map(); // global queue for bot
 
 module.exports = {
     name: "songControl",
@@ -19,6 +19,7 @@ module.exports = {
                 if (!args.length) { // if user does not enter second argument
                     return msg.channel.send("You need to input a second argument!");
                 }
+
                 const song = await findSong(args.join(" "));
                 let list = {}, urlInfo;
 
@@ -35,7 +36,7 @@ module.exports = {
                     msg.channel.send("Cannot find song...");
                 }
 
-                if (!listOfSongs) {
+                if (!listOfSongs) { // establishes a global queue if one does not exist
                     const queueConstructor = {
                         voiceChannel: inVoiceChannel,
                         textChannel: msg.channel,
@@ -46,7 +47,7 @@ module.exports = {
                     musicList.set(msg.guild.id, queueConstructor);
                     queueConstructor.songs.push(list);
 
-                    queueConstructor.connection = await inVoiceChannel.join();
+                    queueConstructor.connection = await inVoiceChannel.join(); // bot joins channel
                     await songPlayer(msg.guild, queueConstructor.songs[0]);
 
                 } else {
@@ -55,44 +56,44 @@ module.exports = {
                 }
                 break;
             case "leave":
-                listOfSongs.songs = [];
-                listOfSongs.connection.dispatcher.end();
+                listOfSongs.songs = []; // empties playlist
+                listOfSongs.connection.dispatcher.destroy(); // ends connection
                 msg.channel.send("Adios!");
                 break;
             case "skip":
                 if (!listOfSongs) {
                     return msg.channel.send("There are **no** songs in the queue!");
                 } else if (listOfSongs.songs.length === 1) {
-                    listOfSongs.connection.dispatcher.end();
+                    listOfSongs.connection.dispatcher.destroy(); // ends connection
                     return msg.channel.send("Adios!")
                 }
-                listOfSongs.connection.dispatcher.end();
+                listOfSongs.connection.dispatcher.end(); // ends current stream dispatcher
                 break;
             case "pause":
                 if (!listOfSongs) {
                     return msg.channel.send("There is no music playing!")
 
-                } else if (listOfSongs.connection.dispatcher.paused) {
+                } else if (listOfSongs.connection.dispatcher.paused) { // song is already paused
                     return msg.channel.send("The song is already paused...");
                 }
-                listOfSongs.connection.dispatcher.pause(true);
+                listOfSongs.connection.dispatcher.pause(true); // pauses current stream dispatcher with silence mode
                 msg.channel.send("Your music is paused!");
                 break;
             case "resume":
                 if (!listOfSongs) {
                     return msg.channel.send("There is no music playing!")
 
-                } else if (!listOfSongs.connection.dispatcher.paused) {
+                } else if (!listOfSongs.connection.dispatcher.paused) { // song is not paused
                     return msg.channel.send("The song is already playing...");
                 }
-                listOfSongs.connection.dispatcher.resume();
+                listOfSongs.connection.dispatcher.resume(); // resumes stream dispatcher
                 msg.channel.send(`***${listOfSongs.songs[0].title}*** is resumed!`);
                 break;
             case "list":
                 if (!listOfSongs) {
                     return msg.channel.send("There is no music playing!")
                 }
-                for (let i = 0; i < listOfSongs.songs.length; i++) {
+                for (let i = 0; i < listOfSongs.songs.length; i++) { // loops through the global queue
                     msg.channel.send(`***${i + 1}.***  __${listOfSongs.songs[i].title}__`);
                 }
                 break;
@@ -112,16 +113,16 @@ module.exports = {
                 }
 
                 let nameDelete = listOfSongs.songs[deleteNum].title;
-                listOfSongs.songs.splice(deleteNum, deleteNum + 1);
+                listOfSongs.songs.splice(deleteNum, deleteNum + 1); // removes song and replaces with the next song
                 msg.channel.send(`Removed ***${nameDelete}*** from the queue list`);
                 break;
             case "shuffle":
                 if (!listOfSongs) {
                     return msg.channel.send("There is no music playing!")
-                } else if (listOfSongs.songs.length === 1) {
-                    return msg.channel.send("There is only one song! Cannot shuffle playlist with one song");
+                } else if (listOfSongs.songs.length === 1 || listOfSongs.songs.length === 2) {
+                    return msg.channel.send("There is only one or two song! Cannot shuffle playlist such little songs!");
                 }
-                shuffleSongs(listOfSongs.songs, listOfSongs.songs.length);
+                shuffleSongs(listOfSongs.songs, listOfSongs.songs.length); // shuffles songs in queue
                 msg.channel.send("Queue is now shuffled!");
                 break;
         }
@@ -140,36 +141,34 @@ function checkURL(str) {
     return verifyURL.test(str); // returns true or false
 }
 
+// plays songs from YouTube
 async function songPlayer(guild, song) {
     const songQueue = musicList.get(guild.id);
 
-    if (!song) {
+    if (!song) { // no songs in queue
         songQueue.voiceChannel.leave();
         musicList.delete(guild.id);
         return;
     }
 
-    const playSong = ytdl(song.url, { filter: 'audioonly' });
+    const playSong = ytdl(song.url, { filter: 'audioonly' }); // searches song on Youtube and convert to MP3
     songQueue.connection.play(playSong, { seek: 0, volume: 1 })
         .on('finish', () => {
-            songQueue.songs.shift();
-            songPlayer(guild, songQueue.songs[0]);
+            songQueue.songs.shift(); // removes song played and shifts to the next song
+            songPlayer(guild, songQueue.songs[0]); // play the next song
+            //  the process repeats until all songs in queue are played
         });
 
     await songQueue.textChannel.send(`Currently playing ***${song.title}***`);
 }
 
+// shuffling songs in the queue list
 function shuffleSongs(listOfSongs, songsLength) {
-    for (let i = songsLength - 1; i > 1; i--) {
+    // Durstenfeld's Shuffle
+    for (let i = songsLength - 1; i > 1; i--) { // shuffle songs in queue except the current song playing
         let j = Math.floor(Math.random() * (i + 1));
         let temp = listOfSongs[i];
         listOfSongs[i] = listOfSongs[j];
         listOfSongs[j] = temp;
     }
 }
-
-
-
-
-
-
